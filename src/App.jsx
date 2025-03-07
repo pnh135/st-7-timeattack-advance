044233984f54987254f33b9c495c002aa7a20e42
nahyun park <pnh13584@gmail.com>
2025-03-07 11:39:23 +0900

    clientprovider 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Router from "./shared/Router";
 
+const queryClent = new QueryClient();
+
 function App() {
-  return <Router />;
+  return (
+    <QueryClientProvider client={queryClent}>
+      <Router />
+    </QueryClientProvider>
+  );
 }
 
 export default App;
